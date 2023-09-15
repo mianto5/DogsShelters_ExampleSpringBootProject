389c984be382fa2733f37889f2170f839f3916d2
mianto5 <tomanovamichaela5@gmail.com>
2023-09-15 12:06:58 +0200

    Initiates SpringBoot project.

---
+++ src/main/java/com/project/dogsshelters/DogsSheltersApplication.java
@@ -0,0 +1,17 @@
+package com.project.dogsshelters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsSheltersApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DogsSheltersApplication.class, args);
+
+		System.out.println("Hello from Dogs Shelters Example Project!");
+
+	}
+
+}
---
+++ src/test/java/com/project/dogsshelters/DogsSheltersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dogsshelters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogsSheltersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
